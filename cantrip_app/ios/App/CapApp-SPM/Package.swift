f4f4a9d69b3ff4c990c8c3b84946da4005f710eb
Dan <danielmillst@gmail.com>
2026-09-03 06:47:16 -0500

    fix: resize mobile viewport for keyboard

--- cantrip_app/ios/App/CapApp-SPM/Package.swift
+++ cantrip_app/ios/App/CapApp-SPM/Package.swift
@@ -17,6 +17,7 @@ let package = Package(
         .package(name: "CapacitorBarcodeScanner", path: "../../../../node_modules/.pnpm/@capacitor+barcode-scanner@3.1.1_@capacitor+core@8.5.0/node_modules/@capacitor/barcode-scanner"),
         .package(name: "CapacitorFilesystem", path: "../../../../node_modules/.pnpm/@capacitor+filesystem@8.1.3_@capacitor+core@8.5.0/node_modules/@capacitor/filesystem"),
         .package(name: "CapacitorHaptics", path: "../../../../node_modules/.pnpm/@capacitor+haptics@8.0.2_@capacitor+core@8.5.0/node_modules/@capacitor/haptics"),
+        .package(name: "CapacitorKeyboard", path: "../../../../node_modules/.pnpm/@capacitor+keyboard@8.0.5_@capacitor+core@8.5.0/node_modules/@capacitor/keyboard"),
         .package(name: "CapacitorNetwork", path: "../../../../node_modules/.pnpm/@capacitor+network@8.0.1_@capacitor+core@8.5.0/node_modules/@capacitor/network"),
         .package(name: "CapacitorShare", path: "../../../../node_modules/.pnpm/@capacitor+share@8.0.1_@capacitor+core@8.5.0/node_modules/@capacitor/share")
     ],
@@ -31,6 +32,7 @@ let package = Package(
                 .product(name: "CapacitorBarcodeScanner", package: "CapacitorBarcodeScanner"),
                 .product(name: "CapacitorFilesystem", package: "CapacitorFilesystem"),
                 .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
+                .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard"),
                 .product(name: "CapacitorNetwork", package: "CapacitorNetwork"),
                 .product(name: "CapacitorShare", package: "CapacitorShare")
             ]
