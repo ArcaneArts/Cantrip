aa8684c21462716419b6e7d9d906d6a6fbffa5d8
Dan <danielmillst@gmail.com>
2026-09-03 06:47:41 -0500

    fix: resize mobile viewport for keyboard (#1647)

--- cantrip_app/capacitor.config.ts
+++ cantrip_app/capacitor.config.ts
@@ -1,10 +1,16 @@
 import type { CapacitorConfig } from "@capacitor/cli";
+import { KeyboardResize } from "@capacitor/keyboard";
 
 const config: CapacitorConfig = {
   appId: "art.cantrip",
   appName: "Cantrip",
   webDir: "dist",
   loggingBehavior: "debug",
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Native,
+    },
+  },
   server: {
     androidScheme: "https",
     hostname: "localhost",
